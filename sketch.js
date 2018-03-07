b02c399cca6dd011a920afed412ecd490515bd0e
adrian santuario <santuario@ciencias.unam.mx>
2018-03-07 12:59:39 -0600

    first commit

---
+++ sketch.js
@@ -0,0 +1,76 @@
+/*
+ *
+ * Klustera
+ * Residente (March 2017)
+ * 
+ * Adrian Santuario
+ *
+ * Description: 
+ * ¿Qué tan similar es Residente a Calle 13? ¿Qué tan diferentes son?
+ * ¿Cuál es el ADN de sus letras?
+ * Empieza a navegar y descubre las diferencias y similitudes
+ *
+ * Un artista evoluciona cuando conoce en verdad las herramientas que utiliza
+ * ¿Cuáles son las herramientas de un compositor? : Las palabras 
+ *
+ */
+
+
+/*
+ *****************************************
+ *****************************************
+ * VARIABLES
+ *****************************************
+ *****************************************
+ */
+
+
+/*
+ *****************************************
+ *****************************************
+ * LIFE CYCLE METHODS
+ *****************************************
+ *****************************************
+ */
+
+function preload() {
+
+
+  //Login
+
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+
+}
+
+
+function setup() {
+
+  createCanvas(displayWidth, displayHeight)
+  initialize();
+
+}
+
+function draw() {
+
+}
+
+
+
+/*
+ *****************************************
+ *****************************************
+ * INITIALIZE METHODS
+ *****************************************
+ *****************************************
+ */
+
+
+function initialize() {
+
+
+
+}
